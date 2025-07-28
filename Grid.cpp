50979093b02574e93d639e2b90831535964e874b
benzhang377644 <benzhang377644@gmail.com>
2025-07-28 15:52:51 -0400

    Grid int, height,constructor, and print function - Ben

--- Grid.cpp
+++ Grid.cpp
@@ -1,5 +1,13 @@
 #include "Grid.h"
+#include <iostream>
+using namespace std;
 
-class Grid() {
+Grid::Grid(int width, int height){
+    width = 100;
+    height = 100;
+}
 
-};
+void::Grid::printGrid(){
+  cout << "Grid Width: " << width << endl;
+  cout << "Grid Height: " << height << endl;
+}
--- Grid.h
+++ Grid.h
@@ -1,6 +1,12 @@
 #pragma once
 
 class Grid {
+private:
+    int width;
+    int height;
 
+public:
+    Grid(int width, int height);
 
+    void printGrid();
 };
