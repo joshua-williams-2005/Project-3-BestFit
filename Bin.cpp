41ef74593083efd9ffe28b5b3ccce13fa7a61e4b
joshua-williams-2005 <williams.joshua@ufl.edu>
2025-07-28 17:46:37 -0400

    standardized whitespace

--- Bin.cpp
+++ Bin.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 using namespace std;
 
-Bin::Bin(){
+Bin::Bin() {
     rows = 50;
     columns = 50;
 }
-void Bin::placetherec(int width, int height, int startrows, int startcol){
-    for(int i=0;i<width;i++){
+
+void Bin::placetherec(int width, int height, int startrows, int startcol) {
+    for(int i=0;i<width;i++) {
         for (int j = 0; j < height; j++)
             a[i + startrows][j + startcol].a = true;
     }
