585da05637dc47c426581523dbbea801540980c4
heimweh17 <71220635+heimweh17@users.noreply.github.com>
2025-07-28 17:08:03 -0400

    bin

--- Bin.h
+++ Bin.h
@@ -1,11 +1,13 @@
 #pragma once
-
+struct newgrid{
+    bool a=false;
+};
 class Bin {
 private:
     int rows;
     int columns;
+    newgrid a[50][50];
 public:
     int capacity;
     Bin();
-
 };
