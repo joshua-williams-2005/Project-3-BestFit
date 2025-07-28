975ada00045d91bc1761e5a9321a2746bed0e03e
benzhang377644 <benzhang377644@gmail.com>
2025-07-28 16:21:24 -0400

    testing - Ben

--- main.cpp
+++ main.cpp
@@ -1,33 +1,23 @@
 #include "Grid.h"
 #include <iostream>
 #include <string>
-using namespace std;
+
 
 int main() {
     std::string userInput;
-    cout << "~~~~~~~~2D Rectangles Packing~~~~~~~~"<<endl;
-    cout << "1. Generate Rectangles" << endl;
-    cout << "2. Run First-Fit Decreasing" << endl;
-    cout << "3. Run Best-Fit Decreasing" << endl;
-    cout << "4. Show Results" << endl;
-    cout << "5. Exit" << endl;
-    cout << "Please Select a Choice Above" << endl;
     while (userInput != "5") {
         std::getline(std::cin, userInput);
         if (userInput == "1") {
-            cout<<"generate rectangles" << endl;
+            //generate rectangles
         }
         else if (userInput == "2") {
-            cout<<"first-fit" << endl;
+            //first-fit
         }
         else if (userInput == "3") {
-            cout<<"best-fit" << endl;
+            //best-fit
         }
         else if (userInput == "4") {
-            cout<<"show results" << endl;
-        }
-        else {
-            cout<<"Please Enter a Number between 1 and 5" << endl;
+            //show results
         }
     }
 }
