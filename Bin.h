94370903cbc072788353427a0832fa5ac1e85011
heimweh17 <71220635+heimweh17@users.noreply.github.com>
2025-07-31 16:02:36 -0400

    Update Bin.h

--- Bin.h
+++ Bin.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <utility>
-
+const int N=5;//for the side length of bins
 struct board {
     bool occupied = false;
 };
@@ -9,10 +9,10 @@ class Bin {
 private:
     int rows;
     int columns;
-    board table[50][50];
+    board table[N][N];
 public:
     Bin();
-    int capacity = 2500;
+    int capacity = N*N;
     std::pair<int, int> canFit(int height, int width);
     bool is_empty();
     bool canPlaceAt(int height, int width, int startRow, int startCol);
