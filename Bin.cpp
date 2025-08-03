a9f1e7317c7036dda1c129992d2971ad82991d2d
joshua-williams-2005 <williams.joshua@ufl.edu>
2025-08-03 19:51:41 -0400

    Removed unused function

--- Bin.cpp
+++ Bin.cpp
@@ -47,41 +47,6 @@ pair<int, int> Bin::canFit(int height, int width) {
     return make_pair(-50, -50);
 }
 
-/*
-pair<int, int> Bin::canFit(int height, int width) {
-    if (height > rows || width > columns) {
-        return make_pair(-50,-50);
-    }
-    const int N=50;
-    bool flag=true;
-    int over=0;
-    int newx=-50;
-    int newy=-50;
-    int i,j;
-    for(i=0;i<N-height;i++){
-        for(j=0;j<N-width;j++){
-            flag=true;
-            for(int k = 0;k< height; ++k) {
-                for (int l = 0; l < width; ++l) {
-                    if (table[i+k][j+l].occupied == true) {
-                        flag=false;
-                    }
-                }
-            }
-            if(flag==true){
-                over=1;
-                newx=i;
-                newy=j;
-                break;
-            }
-        }
-        if(over==1){
-            break;
-        }
-    }
-    return make_pair(newx,newy);
-}*/
-
 void Bin::placeRectangle(int height, int width, int startRow, int startCol) {
     //Verify if placement is valid
     if (!canPlaceAt(height, width, startRow, startCol)) {
